30946816bc10be7abc27b4403d246676221455b6
Vincent <mariosonic1793@gmail.com>
2025-03-11 23:52:19 -0700

    Added divide and first test

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -2,3 +2,7 @@
 int add(int a, int b) {
     return a + b;
 }
+
+int divide(int a, int b) {
+    return a / b;
+}
--- Calculator/calculator.h
+++ Calculator/calculator.h
@@ -1,4 +1,5 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 int add(int a, int b);
+int divide(int a, int b);
 #endif
