1302687bbc5f9531ec9f550f5cce628304f919a3
Vincent <mariosonic1793@gmail.com>
2025-03-11 23:28:46 -0700

    Oh whoops read the hw removing stuff
    Ok I just read the homework a little closer and I realized these are separate commits so here I'll just do this so it's close to how the homework does it (ignore the comments lol)

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -8,6 +8,7 @@ void test_add_positive_numbers(void) {
     TEST_ASSERT_EQUAL(5, add(2, 3)); // 2 + 3 = 5
 }
 
+/*
 void test_add_positive_and_negative_numbers(void) {
     TEST_ASSERT_EQUAL(1, add(5, -4)); // 5 + (-4) = 1
 }
@@ -21,6 +22,7 @@ void test_add_zero(void) {
     TEST_ASSERT_EQUAL(0, add(0, 0));   // 0 + 0 = 0
 }
 
+/*
 void test_add_overflow(void) {
     int result = add(INT_MAX, 1);
     TEST_ASSERT_TRUE(result < 0); // Checks overflow
@@ -30,14 +32,16 @@ void test_add_underflow(void) {
     int result = add(INT_MIN, -1);
     TEST_ASSERT_TRUE(result > 0); // Checks underflow
 }
+*/
 
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
+    /*
     RUN_TEST(test_add_positive_and_negative_numbers);
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
     RUN_TEST(test_add_overflow);
-    RUN_TEST(test_add_underflow);
+    RUN_TEST(test_add_underflow);*/
     return UNITY_END();
 }
